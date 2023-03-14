2121028abb17f20aee5bc3841e6f610e6da753b2
Vasiliev Victor <vasilievvict1@yandex.ru>
2023-03-15 01:57:29 +0300

    added endpoints for getting and adding all components

--- src/main/java/com/xxavierr404/dreambuildr/controllers/CatalogueController.java
+++ src/main/java/com/xxavierr404/dreambuildr/controllers/CatalogueController.java
@@ -1,7 +1,6 @@
 package com.xxavierr404.dreambuildr.controllers;
 
-import com.xxavierr404.dreambuildr.models.CPU;
-import com.xxavierr404.dreambuildr.models.Motherboard;
+import com.xxavierr404.dreambuildr.models.*;
 import com.xxavierr404.dreambuildr.services.CatalogueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -40,6 +39,54 @@ public class CatalogueController {
     @PostMapping("/cpus")
     public ResponseEntity<String> addCPU(CPU cpu) {
         catalogueService.addNewCPU(cpu);
+        return ResponseEntity.ok("Success");
+    }
+
+    @GetMapping("/rams")
+    public List<RAM> getRAMs(@RequestParam("start") Long start,
+                             @RequestParam("end") Long end) {
+        return catalogueService.getRAMsByIdBetween(start, end);
+    }
+
+    @PostMapping("/rams")
+    public ResponseEntity<String> addRAM(RAM ram) {
+        catalogueService.addNewRAM(ram);
+        return ResponseEntity.ok("Success");
+    }
+
+    @GetMapping("/drives")
+    public List<Drive> getDrives(@RequestParam("start") Long start,
+                                 @RequestParam("end") Long end) {
+        return catalogueService.getDrivesByIdBetween(start, end);
+    }
+
+    @PostMapping("/drives")
+    public ResponseEntity<String> addDrive(Drive drive) {
+        catalogueService.addNewDrive(drive);
+        return ResponseEntity.ok("Success");
+    }
+
+    @GetMapping("/powerunits")
+    public List<PowerUnit> getPowerUnits(@RequestParam("start") Long start,
+                                   @RequestParam("end") Long end) {
+        return catalogueService.getPUsByIdBetween(start, end);
+    }
+
+    @PostMapping("/powerunits")
+    public ResponseEntity<String> addPowerUnit(PowerUnit powerUnit) {
+        catalogueService.addNewPowerUnit(powerUnit);
+        return ResponseEntity.ok("Success");
+    }
+
+    @GetMapping("/videocards")
+    public List<Videocard> getVideocards(@RequestParam("start") Long start,
+                             @RequestParam("end") Long end) {
+        return catalogueService.getVideocardsByIdBetween(start, end);
+    }
+
+    @PostMapping("/videocards")
+    public ResponseEntity<String> addVideocard(Videocard videocard) {
+        catalogueService.addNewVideocard(videocard);
         return ResponseEntity.ok("Success");
     }
 }
--- src/main/java/com/xxavierr404/dreambuildr/services/CatalogueService.java
+++ src/main/java/com/xxavierr404/dreambuildr/services/CatalogueService.java
@@ -1,9 +1,7 @@
 package com.xxavierr404.dreambuildr.services;
 
-import com.xxavierr404.dreambuildr.models.CPU;
-import com.xxavierr404.dreambuildr.models.Motherboard;
-import com.xxavierr404.dreambuildr.repositories.CPURepository;
-import com.xxavierr404.dreambuildr.repositories.MotherboardRepository;
+import com.xxavierr404.dreambuildr.models.*;
+import com.xxavierr404.dreambuildr.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +11,24 @@ import java.util.List;
 public class CatalogueService {
     private final MotherboardRepository motherboardRepository;
     private final CPURepository cpuRepository;
+    private final RAMRepository ramRepository;
+    private final DriveRepository driveRepository;
+    private final PowerUnitRepository powerUnitRepository;
+    private final VideocardRepository videocardRepository;
 
     @Autowired
     public CatalogueService(MotherboardRepository motherboardRepository,
-                            CPURepository cpuRepository) {
+                            CPURepository cpuRepository,
+                            RAMRepository ramRepository,
+                            DriveRepository driveRepository,
+                            PowerUnitRepository powerUnitRepository,
+                            VideocardRepository videocardRepository) {
         this.motherboardRepository = motherboardRepository;
         this.cpuRepository = cpuRepository;
+        this.ramRepository = ramRepository;
+        this.driveRepository = driveRepository;
+        this.powerUnitRepository = powerUnitRepository;
+        this.videocardRepository = videocardRepository;
     }
 
     public List<Motherboard> getMotherboardsByIdBetween(Long start, Long end) {
@@ -35,5 +45,37 @@ public class CatalogueService {
 
     public void addNewCPU(CPU cpu) {
         cpuRepository.save(cpu);
+    }
+
+    public List<RAM> getRAMsByIdBetween(Long start, Long end) {
+        return ramRepository.findAllByIdBetween(start, end);
+    }
+
+    public void addNewRAM(RAM ram) {
+        ramRepository.save(ram);
+    }
+
+    public List<Drive> getDrivesByIdBetween(Long start, Long end) {
+        return driveRepository.findAllByIdBetween(start, end);
+    }
+
+    public void addNewDrive(Drive drive) {
+        driveRepository.save(drive);
+    }
+
+    public List<PowerUnit> getPUsByIdBetween(Long start, Long end) {
+        return powerUnitRepository.findAllByIdBetween(start, end);
+    }
+
+    public void addNewPowerUnit(PowerUnit powerUnit) {
+        powerUnitRepository.save(powerUnit);
+    }
+
+    public List<Videocard> getVideocardsByIdBetween(Long start, Long end) {
+        return videocardRepository.findAllByIdBetween(start, end);
+    }
+
+    public void addNewVideocard(Videocard videocard) {
+        videocardRepository.save(videocard);
     }
 }
