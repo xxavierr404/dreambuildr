aa6e201a408d33898658c48637d79f87b0f1108e
Vasiliev Victor <vasilievvict1@yandex.ru>
2023-03-19 23:42:39 +0300

    Started main page, added header and bar with component categories

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./html/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      "logo": ['"Work Sans"', 'sans-serif'],
+      "title": ['Montserrat', 'sans-serif'],
+    }
   },
   plugins: [],
 }
