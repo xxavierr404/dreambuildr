72d8715b157746ba9dd17f1538a9d3a96c9863eb
Vasiliev Victor <vasilievvict1@yandex.ru>
2023-04-01 01:45:18 +0300

    Fixed catalogue page grid

--- src/main/frontend/src/components/Catalogue.tsx
+++ src/main/frontend/src/components/Catalogue.tsx
@@ -27,13 +27,13 @@ const Catalogue = () => {
                     </select>
                     <h3 className="font-title text-2xl font-bold tracking-widest italic">Filter properties</h3>
                 </div>
-                <div className="col-span-3 bg-white rounded-xl grid grid-cols-4 grid-rows-5 gap-5">
+                <div className="catalogue-items-grid">
                     <div className="flex flex-col items-center bg-gradient-to-br
-                    from-blue-600 to-cyan-300 rounded-xl m-3 gap-3 flex-shrink">
-                        <img src="../res/microchip-svgrepo-com.svg" alt="Sample" className="h-1/3 m-3"/>
+                    from-blue-600 to-cyan-300 rounded-xl p-3 m-1 gap-3 h-fit">
+                        <img src="../res/microchip-svgrepo-com.svg" alt="Sample" className="w-1/3"/>
                         <div className="text-white font-title text-xl">Gigabyte B450H</div>
                         <div className="text-white font-title text-lg">8000 руб.</div>
-                        <button className="bg-white text-black font-title font-bold rounded-full ring-2 ring-black
+                        <button className="bg-white text-black font-title text-sm font-bold rounded-full ring-2 ring-black
                         px-3 py-1 transition-all hover:scale-110 hover:-rotate-3">Добавить в корзину</button>
                     </div>
                 </div>
